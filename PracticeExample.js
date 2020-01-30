e8242e8a9a943f26969997d41267868b245e7e17
jshukla <jshukla@INBADJSHUKLA.infor.com>
2020-01-30 16:23:13 +0530

    data

--- PracticeExample.js
+++ PracticeExample.js
@@ -1,49 +1,58 @@
 describe("Example",function(){
+	var using = require('jasmine-data-provider');
+	var obj = require("./pageobjects.js");
+	var d = require("./data.js");
 	
-	var obj = require("./pageobjects.js")
+	
+	
 	beforeEach(function(){
 			obj.getURL()
 			//browser.manage().window().maximize()
 		    browser.waitForAngularEnabled(false)
 		})
-	it("Fucntion1",function(){
 		
-		obj.name.sendKeys("JayaS")
-		obj.email.sendKeys("jaya124shukla@gmail.com")
-		obj.input.sendKeys("jaya")
-		element(by.id("exampleCheck1")).click()
-		obj.gender.click()
-		element.all(by.name("inlineRadioOptions")).first().click()
-		obj.submit.click()
-		obj.successmessage.getText().then(function(text){
-			console.log(text)
-		})
 		
-		element(by.linkText("Shop")).click()
-		element.all(by.tagName("app-card")).each(function(item){
-			item.element(by.css("h4 a")).getText().then(function(text){
-				if(text=="iphone X" || text=="Blackberry"){
-					item.element(by.css("button")).click()
+	using(d.datadriven, function (data, description){
+		it("Fucntion1",function(){
+			
+			obj.name.sendKeys(data.name1)
+			obj.email.sendKeys(data.email1)
+			obj.input.sendKeys(data.id1)
+			element(by.id("exampleCheck1")).click()
+			obj.gender.click()
+			element.all(by.name("inlineRadioOptions")).first().click()
+			obj.submit.click()
+			obj.successmessage.getText().then(function(text){
+				console.log(text)
+			})
+			
+			element(by.linkText("Shop")).click()
+			element.all(by.tagName("app-card")).each(function(item){
+				item.element(by.css("h4 a")).getText().then(function(text){
+					if(text=="iphone X" || text=="Blackberry"){
+						item.element(by.css("button")).click()
+						
+					}
 					
-				}
-				
+				})
+			})
+			
+			browser.sleep(5000)
+			element(by.partialLinkText("Checkout")).getText().then(function(count){
+				var r = count.split("(")
+				console.log(r[1].trim().charAt(0))
+				expect(r[1].trim().charAt(0)).toBe('2')
 			})
 		})
-		
-		browser.sleep(5000)
-		element(by.partialLinkText("Checkout")).getText().then(function(count){
-			var r = count.split("(")
-			console.log(r[1].trim().charAt(0))
-			expect(r[1].trim().charAt(0)).toBe('2')
-		})
-	})
+	});	
+	
 	
 	afterEach(function(){
 		browser.takeScreenshot().then(function(){
 			obj.writepath('png','exception.png')
 		})
 		console.log("Test complete")
-		browser.quit()
+		
 		
 	})
 	
---
+++ data.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+module.exports=
+{
+	datadriven:{
+		
+
+		firstData :
+		{
+				name1 : "jshuk",
+				email1 : "jshuk2011@gmail.com",
+				id1 : "jaya"
+		},
+
+		secondData :
+		{
+			name1 : "jshuk1",
+			email1 : "jshuk12011@gmail.com",
+			id1 : "jaya23"
+		},
+
+		thirdData :{
+			name1 : "jshuk333",
+			email1 : "jshuk20131@gmail.com",
+			id1 : "jaya333"
+		}
+		}
+}
+
